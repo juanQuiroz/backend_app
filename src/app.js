ee9a12255099471c47edd5c7ba427cdf18ceba98
Juan de Dios <jaqp_911@hotmail.com>
2022-05-18 13:40:57 -0500

    git ignore added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/',(req,res) => {
-    res.json({
-        nameapp: "backend_app"
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    nameapp: "backend_app",
+    test: "Hello world from express",
+  });
+});
 
 export default app;
